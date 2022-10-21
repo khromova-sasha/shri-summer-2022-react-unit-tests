6695a5f65f601bbe9ee3f3da099f92645dd2e5c6
sashakhromova <khromova-sasha@inbox.ru>
2022-10-21 13:47:46 +0300

    tests fixes

--- src/components/product-info/ProductInfo.test.jsx
+++ src/components/product-info/ProductInfo.test.jsx
@@ -1,10 +1,9 @@
 import {ProductInfo} from "./ProductInfo";
 import "./ProductInfo.module.css";
 import {render, screen} from "@testing-library/react";
-import {ProductCard} from "../product-card/ProductCard";
 
 describe('Компонент ProductInfo', () => {
-    it("Отображает имя букета",() => {
+    it("Отображает имя букета", () => {
         render(<ProductInfo bouquetHeight={10} bouquetWidth={10} currentPrice={100}
                             flowersCount={10} title={'Букет'} oldPrice={1000}/>);
         expect(screen.queryByText('Букет')).toBeInTheDocument();
@@ -38,16 +37,21 @@ describe('Компонент ProductInfo', () => {
         expect(screen.queryAllByTestId('bouquet-width').length).toBe(1);
     });
 
-    it('Не отображать старую цену, если не задана или 0', () => {
+    it('Не отображать старую цену, если undefined', () => {
         render(<ProductInfo bouquetHeight={10} bouquetWidth={10} currentPrice={100}
                             flowersCount={10} title={'Букет'} oldPrice={undefined}/>);
 
         expect(screen.queryAllByTestId('product-info-old-price').length).toBe(0);
+    });
 
+    it("Не отображать старую цену, если null", () => {
         render(<ProductInfo bouquetHeight={10} bouquetWidth={10} currentPrice={100}
                             flowersCount={10} title={'Букет'} oldPrice={null}/>);
 
         expect(screen.queryAllByTestId('product-info-old-price').length).toBe(0);
+    });
+
+    it("Не отображать старую цену, если 0", () => {
 
         render(<ProductInfo bouquetHeight={10} bouquetWidth={10} currentPrice={100}
                             flowersCount={10} title={'Букет'} oldPrice={0}/>);
@@ -55,16 +59,22 @@ describe('Компонент ProductInfo', () => {
         expect(screen.queryAllByTestId('product-info-old-price').length).toBe(0);
     });
 
-    it('Не отображать количество цветов, если не задано или 0', () => {
+    it('Не отображать количество цветов, если undefined', () => {
         render(<ProductInfo bouquetHeight={10} bouquetWidth={10} currentPrice={100}
                             flowersCount={undefined} title={'Букет'} oldPrice={1000}/>);
 
         expect(screen.queryAllByTestId('flowers-count').length).toBe(0);
+    });
+
+    it("Не отображать количество цветов, если null", () => {
 
         render(<ProductInfo bouquetHeight={10} bouquetWidth={10} currentPrice={100}
                             flowersCount={null} title={'Букет'} oldPrice={1000}/>);
 
         expect(screen.queryAllByTestId('flowers-count').length).toBe(0);
+    });
+
+    it("Не отображать количество цветов, если 0", () => {
 
         render(<ProductInfo bouquetHeight={10} bouquetWidth={10} currentPrice={100}
                             flowersCount={0} title={'Букет'} oldPrice={100}/>);
@@ -72,16 +82,22 @@ describe('Компонент ProductInfo', () => {
         expect(screen.queryAllByTestId('flowers-count').length).toBe(0);
     });
 
-    it('Не отображать высоту букета, если не задана или 0', () => {
+    it('Не отображать высоту букета, если undefined', () => {
         render(<ProductInfo bouquetHeight={undefined} bouquetWidth={10} currentPrice={100}
                             flowersCount={10} title={'Букет'} oldPrice={1000}/>);
 
         expect(screen.queryAllByTestId('bouquet-height').length).toBe(0);
 
+    });
+
+    it("Не отображать высоту букета, если null", () => {
         render(<ProductInfo bouquetHeight={null} bouquetWidth={10} currentPrice={100}
                             flowersCount={10} title={'Букет'} oldPrice={1000}/>);
 
         expect(screen.queryAllByTestId('bouquet-height').length).toBe(0);
+    });
+
+    it("Не отображать высоту букета, если 0", () => {
 
         render(<ProductInfo bouquetHeight={0} bouquetWidth={10} currentPrice={100}
                             flowersCount={10} title={'Букет'} oldPrice={100}/>);
@@ -89,16 +105,22 @@ describe('Компонент ProductInfo', () => {
         expect(screen.queryAllByTestId('bouquet-height').length).toBe(0);
     });
 
-    it('Не отображать ширину букета, если не задана или 0', () => {
+    it('Не отображать ширину букета, если undefined', () => {
         render(<ProductInfo bouquetHeight={10} bouquetWidth={undefined} currentPrice={100}
                             flowersCount={10} title={'Букет'} oldPrice={1000}/>);
 
         expect(screen.queryAllByTestId('bouquet-width').length).toBe(0);
+    });
+
+    it("Не отображать ширину букета, если null", () => {
 
         render(<ProductInfo bouquetHeight={10} bouquetWidth={null} currentPrice={100}
                             flowersCount={10} title={'Букет'} oldPrice={1000}/>);
 
         expect(screen.queryAllByTestId('bouquet-width').length).toBe(0);
+    });
+
+    it("Не отображать ширину букета, если 0", () => {
 
         render(<ProductInfo bouquetHeight={10} bouquetWidth={0} currentPrice={100}
                             flowersCount={10} title={'Букет'} oldPrice={100}/>);
